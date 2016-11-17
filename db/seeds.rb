153292e35dc0002397c444a7632f2850fa6f0d5d
Tara McParland <tara.mcparland@gmail.com>
2016-11-17 01:37:11 +0000

    Seeding and Destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create Users
+10.times do
+  User.create!(
+
+  email:    Faker::Internet.email,
+  password: Faker::Internet.password(8)
+  )
+end
+users = User.all
+
+# Create Items
+100.times do
+   Item.create!(
+     name: Faker::Pokemon.name,
+     user: users.sample
+   )
+ end
+items = Item.all
+ 
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Item.count} items created"
